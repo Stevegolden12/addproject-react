31f56c261c67b997407de7ca42ded10f0b78a02f
Stevegolden12 <sgolden2525@outlook.com>
2018-08-09 00:11:30 -0700

    Saving for night
    Saving for night continuing tomorrow

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Projects extends Component {
+  let projectItems;
+	if(this.props.projects){
+		projectItems = this.props.propjects.map(project => {
+			console.log(project);
+		});
+	}
   render() {
     return (
       <div className="Projects">
---
+++ src/Components/ProjectsItems.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ProjectItems extends Component {
+  render() {
+    return (
+      <li className="Projects">
+			My Projects
+			{this.props.test}
+      </div>
+    );
+  }
+}
+
+export default Projects;
