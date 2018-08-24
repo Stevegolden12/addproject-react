f25d4ed63ee7229d700abf7c5d812094c52a191e
Steve Golden <sgolden2525@outlook.com>
2018-08-23 23:25:36 -0600

    Completed Adding projects and added uuid api
    Finished able to add projects
    Added uuid npm for unique identifers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import './App.css';
 import Projects from './Components/Projects'
 import AddProject from './Components/AddProject'
@@ -6,10 +7,9 @@ import AddProject from './Components/AddProject'
 
 class App extends Component {
 	constructor(){
-	super();
+    super();
 		this.state = {
 			projects: [
-				
 			]
 		}
   }
@@ -18,14 +18,17 @@ class App extends Component {
     this.setState({
       projects: [
         {
+          id: uuid.v4(),
           title: 'Business Website',
           category: 'Web Design'
         },
         {
+          id: uuid.v4(),
           title: 'Social App',
           category: 'Mobile Development'
         },
         {
+          id: uuid.v4(),
           title: 'Ecommerce Shopping Cart',
           category: 'Web Development'
         }
@@ -33,10 +36,18 @@ class App extends Component {
     })
   }
 
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({
+      projects: projects
+    });
+  }
+
   render() {
     return (
       <div className="App">
-			<AddProject />
+        <AddProject addProject={ this.handleAddProject.bind(this)} />
       <Projects projects={this.state.projects}/>
       </div>
     );
